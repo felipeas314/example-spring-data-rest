4d935011f2cfe46b5fed8554df7e75c999f1a49c
Felipe Alexandre <felipe.alexandre@g4flex.com.br>
2017-12-16 09:43:18 -0300

    configurações do arquivos application

--- src/main/java/br/com/example/repository/UserRepository.java
+++ src/main/java/br/com/example/repository/UserRepository.java
@@ -3,6 +3,8 @@ package br.com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import br.com.example.model.User;
+
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Integer>{
 
